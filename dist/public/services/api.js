fa4cad04dca37615bed8a7b5853a5e8efe510542
Ben Kinsey <bkinsey@gmail.com>
2015-09-28 01:09:03 -1000

    jspm fix, git rm dist/*

--- dist/public/services/api.js
+++
@@ -1,9 +0,0 @@
-var API = (function () {
-    function API() {
-    }
-    API.prototype.getNavItems = function () {
-        return fetch('http://localhost:3000/api/nav_items');
-    };
-    return API;
-})();
-exports.API = API;
